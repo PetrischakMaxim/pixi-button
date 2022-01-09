cb9d2686cb0fc69ce868570c5e297d390f9bc5cd
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-01-09 22:13:02 +0200

    refactor: update code

--- js/button.js
+++ js/button.js
@@ -1,96 +1,57 @@
-export default class Button extends PIXI.Container {
+export default class Button extends PIXI.Sprite {
 
     constructor(options = {}) {
         super();
-        this.options = this.setOptions(options);
-        this.init();
+        this._options = options;
+        this._init();
     }
 
-    setOptions(options) {
-        const defaultOptions = {
-            width: 160,
-            height: 40,
-            texture: null,
-            x: 0,
-            y: 0,
-            fill: 0xFF3300,
-            label: 'Default button',
-            labelStyle: {
-                fill: '#ffffff',
-                fontSize: '16px',
-                fontFamily: 'Arial, sans-serif',
-            },
-            disabled: false,
-        };
-
-        return {...defaultOptions, ...options};
-    }
-
-    init() {
-        this.id = this._generateId();
-        this.element = null;
+    _init() {
+        this._setTexture("up");
         this.buttonMode = true;
         this.interactive = true;
-        this.x = this.options.x;
-        this.y = this.options.y;
-        this.disabled = this.options.disabled;
+        this._isEnabled = true;
+    }
 
-        if(this.options.texture) {
-            this.drawTexture(this.options.texture);
-            return;
+    _setTexture(texture) {
+        if (!this._options.textures) {
+            return
         }
-        this.drawGraphics();
+        this.texture = this._options.textures[texture];
     }
 
-    drawTexture(texture) {
-        this.element = new PIXI.Sprite(texture);
-        this.element.width = this.options.width;
-        this.element.height = this.options.height;
-        this.addChild(this.element);
+    _updateTexture(texture) {
+        this.texture = texture;
     }
 
-    drawGraphics() {
-        this.element = new PIXI.Graphics();
-        this.element.x = this.x;
-        this.element.y = this.y;
-        this.element.beginFill(this.options.fill);
-        this.element.drawRect(0, 0, this.options.width, this.options.height);
-        this.element.endFill();
-
-        if(this.options.label) {
-            this.addLabel();
-        }
-        this.addChild(this.element);
-    }
-
-    addLabel() {
-        this.label = new PIXI.Text(this.options.label, this.options.labelStyle);
+    _addLabel() {
+        this.label = new PIXI.Text(this.options.label.text, this.options.label.style);
         this.label.anchor.set(0.5);
-        this.label.x = this.element.width / 2;
-        this.label.y = this.element.height / 2;
-        this.element.addChild(this.label);
+        this.label.x = this.width / 2;
+        this.label.y = this.height / 2;
+        this.addChild(this.label);
     }
 
-    updateLabel(label) {
-        this.label.text = label;
+    _checkDisabledState(isEnabled) {
+        if(!isEnabled) {
+            this._setTexture("disabled");
+        } else {
+            this._setTexture("up");
+        }
+        this._isEnabled = isEnabled;
+        this.buttonMode = isEnabled;
+        this.interactive = isEnabled;
     }
 
-    updateTexture(texture = this.options.texture) {
-        this.element.texture = texture;
+    setCallback(callback) {
+        callback();
     }
 
-    setDisabled() {
-       this.disabled = true;
-       this.interactive = false;
-       this.alpha = 0.5;
+    disable() {
+        this._checkDisabledState(false);
     }
 
-    addAction(actionType, callback) {
-        this.on(actionType, callback);
-        return this;
-    }
-
-    _generateId() {
-        return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    enable() {
+        this._checkDisabledState(true);
     }
 }
