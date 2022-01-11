53723545c41b0d34a0507afab74562e297eacdeb
Maksim Petrishchak <maksim.petrishchak@insigit.com>
2022-01-11 21:01:09 +0200

    refactor: add private,public methods

--- js/button.js
+++ js/button.js
@@ -1,5 +1,4 @@
 export default class Button extends PIXI.Sprite {
-
     constructor(options = {}) {
         super();
         this._options = options;
@@ -7,51 +6,88 @@ export default class Button extends PIXI.Sprite {
     }
 
     _init() {
-        this._setTexture("up");
         this.buttonMode = true;
-        this.interactive = true;
-        this._isEnabled = true;
+        this._isActive = false;
+        this._isHover = false;
+        this._switchEnabledState(true);
+        this._setEvents();
+        if (this._options.label) {
+            this._addLabel();
+        }
     }
 
-    _setTexture(texture) {
-        if (!this._options.textures) {
-            return
+    _setEvents() {
+        this.on("pointerover", this._onHover)
+            .on("pointerout", this._onOut)
+            .on("pointerdown", this._onDown)
+            .on("pointerup", this._onUp)
+            .on("pointerupoutside", this._onUp);
+    }
+
+    _onHover() {
+        this._isHover = true;
+        if(this._isActive) {
+            return;
+        }
+        this._updateTexture("hover");
+    }
+
+    _onOut() {
+        this._isHover = false;
+        if(this._isActive) {
+            return;
+        }
+        this._updateTexture("default");
+    }
+
+    _onDown() {
+        this._isActive = true;
+        this._updateTexture("down");
+    }
+
+    _onUp() {
+        this._isActive = false;
+        if(this._isHover) {
+            this._updateTexture("hover");
+            return;
+        }
+        this._updateTexture("default");
+    }
+
+    _switchEnabledState(isEnabled) {
+        if (!isEnabled) {
+            this._updateTexture("disabled");
+        } else {
+            this._updateTexture("default");
+        }
+        this._isEnabled = isEnabled;
+        this.interactive = isEnabled;
+    }
+
+    _updateTexture(texture) {
+        if (!this._options.textures[texture]) {
+            return;
         }
         this.texture = this._options.textures[texture];
     }
 
-    _updateTexture(texture) {
-        this.texture = texture;
-    }
-
     _addLabel() {
-        this.label = new PIXI.Text(this.options.label.text, this.options.label.style);
-        this.label.anchor.set(0.5);
-        this.label.x = this.width / 2;
-        this.label.y = this.height / 2;
-        this.addChild(this.label);
-    }
-
-    _checkDisabledState(isEnabled) {
-        if(!isEnabled) {
-            this._setTexture("disabled");
-        } else {
-            this._setTexture("up");
-        }
-        this._isEnabled = isEnabled;
-        this.buttonMode = isEnabled;
-        this.interactive = isEnabled;
+        this._label = new PIXI.Text(this._options.label.text, this._options.label.style);
+        this._label.anchor.set(0.5);
+        this._label.x = this.width / 2;
+        this._label.y = this.height / 2;
+        this.addChild(this._label);
     }
 
     setCallback(callback) {
-        callback();
+        this.on("pointertap", callback);
     }
 
     disable() {
-        this._checkDisabledState(false);
+        this._switchEnabledState(false);
     }
 
     enable() {
-        this._checkDisabledState(true);
+        this._switchEnabledState(true);
     }
 }
