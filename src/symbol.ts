f278883581e98ab980fd519ad4a5a0b8435c13de
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-01-24 20:55:17 +0200

    refactor: update class Symbol

--- src/symbol.ts
+++ src/symbol.ts
@@ -11,55 +11,51 @@ interface SlotSymbol {
 
 export default class Symbol extends Sprite implements SlotSymbol {
 
-    private _startPosition: {x:number, y: number};
+    private _startPosition: { x: number, y: number };
 
-    constructor(texture: Texture) {
+    constructor(texture: Texture, x: number, y: number, size: number) {
         super(texture);
+        this.x = x;
+        this.y = y;
+        this.anchor.set(0.5);
+        this.scale.x = this.scale.y = Math.min(size / this.width, size / this.height);
+        this._startPosition = {x, y};
     }
 
     public startBounce(): Promise<void> {
         return new Promise((resolve) => {
-            gsap.to(this,
-                {
-                    duration: getRandomInt(1, 2),
-                    y: this.y - getRandomInt(5, 50),
-                    ease: "bounce",
-                    onComplete: resolve
-                }
-            );
+            gsap.to(this, {
+                duration: getRandomInt(1, 2),
+                y: this.y - getRandomInt(5, 50),
+                ease: "bounce",
+                onComplete: resolve
+            });
         });
     }
 
     public moveOneSlot(): Promise<void> {
         return new Promise((resolve) => {
-            gsap.to(this,
-                {
-                    duration: getRandomInt(0.5, 1.5),
-                    y: this.y - this.height,
-                    onComplete: resolve
-                }
-            );
+            gsap.to(this, {
+                duration: getRandomInt(0.5, 1.5),
+                y: this.y - this.height,
+                ease: "linear",
+                onComplete: resolve
+            });
         });
     }
 
     public endBounce(): Promise<void> {
         return new Promise((resolve) => {
-            gsap.to(this,
-                {
-                    duration: getRandomInt(0.5, 1.5),
-                    y: this._startPosition.y,
-                    ease: "bounce",
-                    onComplete: resolve
-                }
-            );
+            gsap.to(this, {
+                duration: getRandomInt(0.5, 1.5),
+                y: this._startPosition.y,
+                ease: "bounce",
+                onComplete: resolve
+            });
         });
     }
 
     public setSymbol(name: Texture): void {
         this.texture = name;
-    }
-
-    public saveStartPosition(x: number, y: number) {
-        this._startPosition = {x, y};
     }
 }
