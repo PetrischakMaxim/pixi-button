ddb60a56c5265fb8f44cd81c721df5d3005873de
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-02-19 14:21:33 +0200

    refactor: add config to public methods

--- src/symbol.ts
+++ src/symbol.ts
@@ -3,19 +3,13 @@ import {gsap} from "gsap";
 import TweenVars = gsap.TweenVars;
 
 interface SlotSymbol {
-    startBounce(): Promise<void>;
+    startBounce(config: Partial<TweenVars>): Promise<void>;
 
-    moveOneSlot(): Promise<void>;
+    moveOneSlot(config: Partial<TweenVars>): Promise<void>;
 
-    endBounce(): Promise<void>;
+    endBounce(config: Partial<TweenVars>): Promise<void>;
 
     setSymbol(name: Texture): void;
-}
-
-interface AnimationConfig {
-    start: Partial<TweenVars>,
-    move: Partial<TweenVars>,
-    end: Partial<TweenVars>,
 }
 
 interface Options {
@@ -25,60 +19,36 @@ interface Options {
         x: number,
         y: number,
     }
-    animationDuration: number,
 }
 
 export default class Symbol extends Sprite implements SlotSymbol {
 
-    private _animationConfig: AnimationConfig;
-    private readonly _size: number;
-    private _animationDuration: number;
-    private _startPosition: {
+    public startPosition: {
         x: number,
         y: number,
     };
 
+    public size: number;
+
     constructor(options: Options) {
         super(options.texture);
-        this._size = options.size;
-        this._startPosition = options.position;
+        this.startPosition = options.position;
+        this.size = options.size;
         this.position.set(options.position.x, options.position.y);
         this.anchor.set(0.5);
-        this.scale.x = this.scale.y = Math.min(this._size / this.width, this._size / this.height);
-        this._addAnimation(options.animationDuration, this._size, this._startPosition.y);
+        this.scale.x = this.scale.y = Math.min(this.size / this.width, this.size / this.height);
     }
 
-    private _addAnimation(duration: number, size: number, posY: number) {
-        this._animationDuration = duration;
-        this._animationConfig = {
-            start: {
-                duration: this._animationDuration,
-                y: `-=${size}`,
-                repeat: 0,
-                yoyo: true,
-            },
-            move: {
-                duration: this._animationDuration,
-                y: `-=${size}`,
-            },
-            end: {
-                duration: this._animationDuration,
-                y: posY,
-                ease: "bounce",
-            }
-        }
+    public async startBounce(config: Partial<TweenVars>) {
+        await gsap.to(this, config);
+    };
+
+    public async moveOneSlot(config: Partial<TweenVars>) {
+        await gsap.to(this, config);
     }
 
-    public async startBounce() {
-        await gsap.to(this, this._animationConfig.start);
-    }
-
-    public async moveOneSlot() {
-        await gsap.to(this, this._animationConfig.move);
-    }
-
-    public async endBounce() {
-        await gsap.to(this, this._animationConfig.end);
+    public async endBounce(config: Partial<TweenVars>) {
+        await gsap.to(this, config);
     }
 
     public setSymbol(name: Texture): void {
