5963f93efae0a2aede7e72c5b450c41726c1d3ae
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-02-23 22:09:21 +0200

    refactor: add public and private methods, update class

--- src/reel.ts
+++ src/reel.ts
@@ -1,36 +1,55 @@
 import {Container} from "pixi.js";
-
-interface Options {
-    symbolsCount: number;
-    animationSpeed: number;
-    moveCount: number;
-}
+import Symbol from "./symbol";
 
 export default class Reel extends Container {
 
-    private _options: Options;
-    public isAnimated: boolean;
+    private _symbolsBeforeStop: number;
+    public symbolsCount: number;
+    public symbols: Array<Symbol>;
 
-    constructor(options: Options) {
+    constructor(symbolsCount: number) {
         super();
-        this._options = {...options};
-        this.isAnimated = false;
+        this.symbolsCount = symbolsCount;
+        this.symbols = [];
     }
 
-    public async moveSlots(callback: Function) {
-        for (let i = 0; i < this._options.moveCount; i++) {
-            await callback();
+    private async _doSpin() {
+        await this._moveOneSlot();
+        this._symbolsBeforeStop--;
+
+        if (this._symbolsBeforeStop > 0) {
+            await this._doSpin();
+        } else {
+            await Promise.all(this.symbols.map(sym => sym.endBounce()));
         }
     }
 
-    public addSymbols(callback: Function) {
-        for (let i = 1; i <= this._options.symbolsCount; i++) {
-            const symbol = callback(i);
+    private _removeLastSymbol() {
+        const lastSymbol = this.symbols.pop();
+        this.symbols.unshift(lastSymbol);
+    }
+
+    private _changeSymbolsPosition() {
+        this.symbols.forEach((sym,i) => sym.y = sym.size * i + sym.size);
+    }
+
+    private async _moveOneSlot() {
+        await Promise.all(this.symbols.map(sym => sym.moveOneSlot()));
+        this._removeLastSymbol();
+        this._changeSymbolsPosition();
+    }
+
+    public addSymbols(create: Function) {
+        for (let i = 1; i <= this.symbolsCount; i++) {
+            const symbol = create(i);
             this.addChild(symbol);
+            this.symbols.push(symbol);
         }
     }
 
-    get animationSpeed() {
-        return this._options.animationSpeed;
+    public async start(symNumToSpin: number) {
+        this._symbolsBeforeStop = symNumToSpin;
+        await Promise.all(this.symbols.map(sym => sym.startBounce()));
+        await this._doSpin();
     }
 }
